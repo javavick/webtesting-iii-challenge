e2c6e553514ed29038033b146430ff6587c82847
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-12-06 21:59:55 -0400

    Add tests for Dashboard.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+import "jest-dom/extend-expect.js";
+
+import Dashboard from "./Dashboard";
+
+test("Dashboard.js renders properly", () => {
+  expect(render(<Dashboard />)).toMatchSnapshot();
+});
+
+test("Display component renders properly", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText("Unlocked")).toBeInTheDocument();
+});
+
+test("Controls component renders properly", () => {
+  const { getByText } = render(<Dashboard />);
+  expect(getByText("Close Gate")).toBeInTheDocument();
+});
