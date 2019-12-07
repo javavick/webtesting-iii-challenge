1ba44535f83b579f5e745bed7b1117214337ee65
Vick Javariz <51038821+javavick@users.noreply.github.com>
2019-12-06 21:30:11 -0400

    Add tests for Controls.js

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,62 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect.js";
+import Controls from "./Controls.js";
+
+test("Controls.js renders properly", () => {
+  expect(render(<Controls />)).toMatchSnapshot();
+});
+
+test("Displays Lock and Close Gate buttons", () => {
+  const { getByText } = render(<Controls />);
+
+  expect(getByText("Lock Gate")).toBeInTheDocument();
+  expect(getByText("Close Gate")).toBeInTheDocument();
+});
+
+test("Displays Unlock and Open Gate buttons", () => {
+  const { getByText } = render(<Controls locked={true} closed={true} />);
+
+  expect(getByText("Unlock Gate")).toBeInTheDocument();
+  expect(getByText("Open Gate")).toBeInTheDocument();
+});
+
+test("Check if toggleLocked was fired", () => {
+  const toggleLockedMock = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleLocked={toggleLockedMock} locked={false} closed={true} />
+  );
+
+  const lockButton = getByText("Lock Gate");
+
+  fireEvent.click(lockButton);
+  expect(toggleLockedMock).toHaveBeenCalled();
+});
+
+test("Check if toggleClosed was fired", () => {
+  const toggleClosedMock = jest.fn();
+
+  const { getByText } = render(
+    <Controls toggleClosed={toggleClosedMock} locked={false} closed={false} />
+  );
+
+  const closeButton = getByText("Close Gate");
+
+  fireEvent.click(closeButton);
+  expect(toggleClosedMock).toHaveBeenCalled();
+});
+
+test("Check if Close button is disabled if the Gate is locked", () => {
+  const { getByText } = render(<Controls locked={true} />);
+
+  expect(getByText("Close Gate")).toBeDisabled();
+});
+
+test("Check if Locked button is disabled if the Gate is open", () => {
+  const { getByText } = render(<Controls closed={false} />);
+
+  expect(getByText("Lock Gate")).toBeDisabled();
+});
+
+// test("");
